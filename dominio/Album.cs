a8ef2e600c69d7879744d011a52687413e304b7b
RodrigoRuiz <farid.ruiz2000@gmail.com>
2024-01-10 10:39:23 -0300

    first commit

---
+++ dominio/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Album
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Canciones { get; set; }
+        public int FechaCreacion { get; set; }
+        public Autor Autor { get; set; }
+        public Genero Genero { get; set; }
+        public string ImagenUrl { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
